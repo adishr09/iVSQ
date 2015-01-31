c5117d4b92dff3e914ba1bd6bade726d5f489c67
archangel <adi.shr09@gmail.com>
2015-01-31 08:12:55 +0530

    lol

--- 3.php
+++ 3.php
@@ -34,7 +34,17 @@ header( 'Location: 1.php' ) ;
 <div class="main_div">
 <div class="content">
 	<center><h3> VSQ 2015</h3></center>
-	<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan, orci consequat faucibus varius, nulla ex elementum enim, vel fringilla ligula sapien ut sapien. Nunc non mauris elementum, volutpat turpis at, molestie lorem. Maecenas libero felis, pellentesque volutpat nisi ut, euismod tempus metus. Curabitur eu enim ac metus tempor convallis a sed eros. Mauris blandit, nibh vitae porttitor suscipit, tellus orci semper quam, et pellentesque dui diam ut metus. Nunc molestie, risus ac maximus condimentum, est odio ultrices velit, sed suscipit urna leo sit amet eros. Maecenas vel sem hendrerit, dignissim arcu a, aliquam ligula. Nullam molestie pharetra tincidunt. Nulla aliquet ac enim ac venenatis. Pellentesque in  </p>
+	<p> VSQ is back! 
+It is the 8th Edition. :D</p>
+
+<p>The IEEE Student Branch of BVCOE will be conducting the 8th edition of The Dr. Vikram Sarabhai Rotating Shield inter-college quiz in January 2015. </p>
+
+<p>It is a GENERAL QUIZ.</p>
+
+<p>The Quiz is open to all College and School students.
+It is an engrossing and stimulating event that lives up to its billing as one of Delhi's top inter-college quizzes.</p>
+
+<p>Potential topics : Current affairs, history, sports, entertainment (Game of Thrones too, yes), pop culture (no One Direction, no worries!). Basically life, the universe and everything that makes it. </p></p>
 </div>
 <div style="border:groove;border-color:grey;height:75%;position:inherit;float:left;margin-top:5%;"></div>
 <div id="login">
